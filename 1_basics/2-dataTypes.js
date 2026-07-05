85b9babcf7416daf4839f1bc2c998aa9ea4de130
Sarfraz Ali soomro <115340904+Sarfraz94@users.noreply.github.com>
2026-07-05 04:37:30 +0000

    learn about datatypes

---
+++ 1_basics/2-dataTypes.js
@@ -0,0 +1,31 @@
+("use strict");
+// enables Strict Mode in JavaScript, helping catch common coding mistakes and making code safer and more reliable.
+
+/*console.log("sarfraz") console.log("94")
+JavaScript not need to semicomma but sometimes it need for readability
+
+console.log("sarfraz"); console.log("94")
+accurate now work it.*/
+
+// JavaScript Data Types
+// │
+// ├── Primitive (7)
+// │   ├── String
+// │   ├── Number
+// │   ├── Boolean
+// │   ├── Undefined
+// │   ├── Null
+// │   ├── BigInt
+// │   └── Symbol
+// │
+// └── Non-Primitive (Reference)
+//     ├── Object
+//     ├── Array
+//     └── Function
+
+
+var a;
+var b="123"
+
+console.log(typeof(a))
+console.log(typeof(b))
