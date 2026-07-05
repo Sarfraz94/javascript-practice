9d5c260febeab1406b98346c1bab110181ea77ad
Sarfraz Ali soomro <115340904+Sarfraz94@users.noreply.github.com>
2026-07-05 03:37:27 +0000

    learn about consonant and variable

---
+++ basic-1/cost_var.js
@@ -0,0 +1,12 @@
+const homeHead = "male"
+var husband ="doctor"
+let wife ="house wife"
+income=100000
+
+// const homeHead=200  not work bcz it can not override 
+husband="Engineer"
+// it override first varible
+
+
+console.log(homeHead)
+console.table([husband,wife,income])
